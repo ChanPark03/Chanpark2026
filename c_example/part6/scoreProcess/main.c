d733e5cfc2d9344d5fd2772c097980eb78b7c7f6
ChanPark3 <parkchan0302@gmail.com>
2026-03-17 17:17:55 +0900

    구조체와 파일 읽기

---
+++ c_example/part6/scoreProcess/main.c
@@ -0,0 +1,43 @@
+#include<scoreProcess.h>
+
+int main(void) {
+    char *finpath = "/home/chan/Chanpark2026/c_example/part6/scoreProcess/score.dat";
+    char *foutpath ="/home/chan/Chanpark2026/c_example/part6/scoreProcess/score.out";
+    // /home/chan/Chanpark2026/c_example/part6/scoreProcess
+    FILE *fin = fopen (finpath, "r");
+    FILE *fout = fopen (foutpath, "w");
+    int n;
+    
+    
+    printf("처리할 학생의 수를 입력하세요:");
+    if (scanf("%d", &n) != 1)
+        return 1;
+
+    if (fin == NULL || fout == NULL){
+        fprintf(stderr, "파일을 열수 없습니다.\n");
+        return 1;
+    }
+
+    Sdata *students = (Sdata *)malloc(sizeof(Sdata)* n);
+    Sdata **table = (Sdata **)malloc(sizeof(Sdata)* n);
+
+    if(students == NULL || table == NULL){
+        fprintf(stderr, "메모리 할당 실패 \n");
+        return 1;
+    }
+    inputData(fin, students, n);
+    calculateScore(students, n);
+    calculateRank(students, n);
+     sortPointers(students, table, n);
+    printResult(fout, table, n);
+    
+    free(table);
+    free(students);
+   
+
+    
+    fclose(fin);
+    fclose (fout);
+    return 0;
+}
+
