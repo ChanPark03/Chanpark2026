725d31ddb4238b11234c8249b801585ac0bc13f9
ChanPark3 <parkchan0302@gmail.com>
2026-03-23 17:20:09 +0900

    2026-03-23정리

---
+++ atmega128/src/led3.c
@@ -0,0 +1,23 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main()
+{
+    unsigned char led_data = 0x00; // 1byte = 8 bit , 4bit 2^4 = 16 00 ~ ff
+    //unsigned char led_data = 0b00000000; // 0 or 1
+    //uint8_t led_data = 0x00; 위의 두줄과 똑같이 작동함 8번이 가장 잘쓴 코드 
+
+    DDRC = 0x0F;
+    //DDRC = 0b00001111;
+    while (1)
+    {
+        PORTC = led_data;
+        led_data++;
+        if(led_data > 0x0F)
+            led_data = 0;
+            _delay_ms(500);
+    }
+    return 0;
+
+    }
+    
